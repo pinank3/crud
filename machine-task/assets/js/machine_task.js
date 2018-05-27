c050b2a587f02286dbd741b02f58cd50d004050a
pinank3 <37895606+pinank3@users.noreply.github.com>
2018-05-28 03:10:19 +0530

    Add files via upload

---
+++ machine-task/assets/js/machine_task.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+
+	$(".btn-login").click(function(){
+		// alert(111)
+		record = $('#login_form').serialize();
+		$.post("../controllers/login-action.php",record).success(
+			function(response){
+				console.log(response)
+				if(response == "ok"){
+					window.location.href="index.php";
+				}
+				else{
+				$(".msg_login").html(response);
+				}
+			})
+	})
+
+	$(".btn-register").click(function(){
+		record = $('#register_form').serialize();
+		$.post("../controllers/register-action.php",record).success(
+			function(response){
+				$(".msg_register").html(response);
+			})
+	})
+	
+})
